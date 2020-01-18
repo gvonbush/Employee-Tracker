bed9cfde614db0ca116c8452010a24979a54073f
geeveebee <gvonbush@me.com>
2020-01-18 14:13:32 -0500

    added sql file

---
+++ employeetracker.sql
@@ -0,0 +1,31 @@
+
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+
+CREATE DATABASE employee_tracker_db;
+
+
+USE employee_tracker_db;
+
+
+CREATE TABLE department (
+  id int AUTO_INCREMENT NOT NULL,
+  name varchar(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id int AUTO_INCREMENT NOT NULL,
+  title varchar(30) NOT NULL,
+  salary decimal (10,2),
+  department_id INT NOT NULL,
+  PRIMARY KEY(id)
+);
+CREATE TABLE employee (
+  id int AUTO_INCREMENT NOT NULL,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id int NOT NULL,
+  manager_id int,
+  PRIMARY KEY(id)
+);
